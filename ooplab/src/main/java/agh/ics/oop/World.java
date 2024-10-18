14ad4b96ed23af4beef794422a8b13bc5c29f39f
Jerzy Boksa <jerzyxboksa@gmail.com>
2024-10-18 14:03:09 +0200

    implement app

--- ooplab/src/main/java/agh/ics/oop/World.java
+++ ooplab/src/main/java/agh/ics/oop/World.java
@@ -1,7 +1,29 @@
 package agh.ics.oop;
 
-public class World {
-  public static void main(String[] args) {
+import agh.ics.oop.mappers.OptionsParser;
+import agh.ics.oop.model.MoveDirection;
 
+public class World {
+  private static final String GO_FORWARD_MESSAGE = "Zwierzak idzie do przodu";
+  private static final String GO_BACKWARD_MESSAGE = "Zwierzak idzie do tyłu";
+  private static final String GO_LEFT_MESSAGE = "Zwierzak idzie w lewo";
+  private static final String GO_RIGHT_MESSAGE = "Zwierzak idzie w prawo";
+
+  public static void main(String[] args) {
+    System.out.println("Start");
+    run(OptionsParser.parse(args));
+    System.out.println("Stop");
+
+  }
+
+  public static void run(MoveDirection[] directions) {
+    for (MoveDirection direction : directions) {
+      switch (direction) {
+        case FORWARD -> System.out.println(GO_FORWARD_MESSAGE);
+        case BACKWARD -> System.out.println(GO_BACKWARD_MESSAGE);
+        case RIGHT -> System.out.println(GO_RIGHT_MESSAGE);
+        case LEFT -> System.out.println(GO_LEFT_MESSAGE);
+      }
+    }
   }
 }
