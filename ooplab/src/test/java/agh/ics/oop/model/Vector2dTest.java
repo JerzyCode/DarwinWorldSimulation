f732dda14374416869e101daa115a4d0245581f6
Jerzy Boksa <jerzyxboksa@gmail.com>
2024-11-02 10:54:26 +0100

    refactor: vector tests

--- ooplab/src/test/java/agh/ics/oop/model/Vector2dTest.java
+++ ooplab/src/test/java/agh/ics/oop/model/Vector2dTest.java
@@ -1,70 +1,58 @@
 package agh.ics.oop.model;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.Random;
 import java.util.stream.Stream;
 
 class Vector2dTest {
 
-  private static final Random RANDOM = new Random();
-  private static final int MAX_RANDOM = 100000;
-
-  @Test
-  void shouldAddVectors() {
+  @ParameterizedTest
+  @MethodSource("provideAddVectorsTestCases")
+  void shouldAddVectors(Vector2d vector1, Vector2d vector2, Vector2d expected) {
     //given
-    var vector1 = new Vector2d(RANDOM.nextInt(MAX_RANDOM), RANDOM.nextInt(MAX_RANDOM));
-    var vector2 = new Vector2d(RANDOM.nextInt(MAX_RANDOM), RANDOM.nextInt(MAX_RANDOM));
-
     //when
     var result = vector1.add(vector2);
 
     //then
-    Assertions.assertEquals(vector1.getX() + vector2.getX(), result.getX());
-    Assertions.assertEquals(vector1.getY() + vector2.getY(), result.getY());
+    Assertions.assertEquals(expected.getX(), result.getX());
+    Assertions.assertEquals(expected.getY(), result.getY());
   }
 
-  @Test
-  void shouldSubtractVectors() {
+  @ParameterizedTest
+  @MethodSource("provideSubtractVectorsTestCases")
+  void shouldSubtractVectors(Vector2d vector1, Vector2d vector2, Vector2d expected) {
     //given
-    var vector1 = new Vector2d(RANDOM.nextInt(MAX_RANDOM), RANDOM.nextInt(MAX_RANDOM));
-    var vector2 = new Vector2d(RANDOM.nextInt(MAX_RANDOM), RANDOM.nextInt(MAX_RANDOM));
-
     //when
     var result = vector1.subtract(vector2);
 
     //then
-    Assertions.assertEquals(vector1.getX() - vector2.getX(), result.getX());
-    Assertions.assertEquals(vector1.getY() - vector2.getY(), result.getY());
+    Assertions.assertEquals(expected.getX(), result.getX());
+    Assertions.assertEquals(expected.getY(), result.getY());
   }
 
-  @Test
-  void shouldReturnVectorWithOppositeCoordinates() {
+  @ParameterizedTest
+  @MethodSource("provideOppositeTestCases")
+  void shouldReturnVectorWithOppositeCoordinates(Vector2d vector, Vector2d expected) {
     //given
-    var vector = new Vector2d(RANDOM.nextInt(MAX_RANDOM), RANDOM.nextInt(MAX_RANDOM));
-
     //when
     var result = vector.opposite();
 
     //then
-    Assertions.assertEquals(-vector.getX(), result.getX());
-    Assertions.assertEquals(-vector.getY(), result.getY());
+    Assertions.assertEquals(expected.getX(), result.getX());
+    Assertions.assertEquals(expected.getY(), result.getY());
   }
 
-  @Test
-  void shouldReturnToString() {
+  @ParameterizedTest
+  @MethodSource("provideToStringTestCases")
+  void shouldReturnToString(Vector2d vector, String expected) {
     //given
-    var vector = new Vector2d(RANDOM.nextInt(MAX_RANDOM), RANDOM.nextInt(MAX_RANDOM));
-
     //when
     var result = vector.toString();
 
     //then
-    var expected = String.format("(%d, %d)", vector.getX(), vector.getY());
     Assertions.assertEquals(expected, result);
   }
 
@@ -125,6 +113,40 @@ class Vector2dTest {
     Assertions.assertEquals(expected, result);
   }
 
+  private static Stream<Arguments> provideAddVectorsTestCases() {
+    return Stream.of(
+        Arguments.of(new Vector2d(0, 0), new Vector2d(0, 0), new Vector2d(0, 0)),
+        Arguments.of(new Vector2d(1, 1), new Vector2d(2, 2), new Vector2d(3, 3)),
+        Arguments.of(new Vector2d(1, 10), new Vector2d(23, 2), new Vector2d(24, 12)),
+        Arguments.of(new Vector2d(-2, 13), new Vector2d(-4, -10), new Vector2d(-6, 3)),
+        Arguments.of(new Vector2d(2, -13), new Vector2d(-4, 10), new Vector2d(-2, -3)));
+  }
+
+  private static Stream<Arguments> provideSubtractVectorsTestCases() {
+    return Stream.of(
+        Arguments.of(new Vector2d(0, 0), new Vector2d(0, 0), new Vector2d(0, 0)),
+        Arguments.of(new Vector2d(1, 1), new Vector2d(2, 2), new Vector2d(-1, -1)),
+        Arguments.of(new Vector2d(1, 10), new Vector2d(23, 2), new Vector2d(-22, 8)),
+        Arguments.of(new Vector2d(-2, 13), new Vector2d(-4, -10), new Vector2d(2, 23)),
+        Arguments.of(new Vector2d(2, -13), new Vector2d(-4, 10), new Vector2d(6, -23)));
+  }
+
+  private static Stream<Arguments> provideOppositeTestCases() {
+    return Stream.of(
+        Arguments.of(new Vector2d(0, 0), new Vector2d(0, 0)),
+        Arguments.of(new Vector2d(1, 2), new Vector2d(-1, -2)),
+        Arguments.of(new Vector2d(-1, 2), new Vector2d(1, -2)),
+        Arguments.of(new Vector2d(-1, -2), new Vector2d(1, 2)),
+        Arguments.of(new Vector2d(1, -2), new Vector2d(-1, 2)));
+  }
+
+  private static Stream<Arguments> provideToStringTestCases() {
+    return Stream.of(
+        Arguments.of(new Vector2d(0, 0), "(0, 0)"),
+        Arguments.of(new Vector2d(1, 2), "(1, 2)"),
+        Arguments.of(new Vector2d(-1, -2), "(-1, -2)"));
+  }
+
   private static Stream<Arguments> provideEqualTestCases() {
     Vector2d vector = new Vector2d(1, 2);
     return Stream.of(
@@ -183,4 +205,5 @@ class Vector2dTest {
         Arguments.of(new Vector2d(-4, -4), new Vector2d(1, 3), new Vector2d(-4, -4))
     );
   }
+
 }
