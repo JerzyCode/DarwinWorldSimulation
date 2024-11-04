5b5d608709b2f2351a1941b2098fa46ef3ecc3cc
Jerzy Boksa <jerzyxboksa@gmail.com>
2024-11-04 21:05:20 +0100

    feat: add orientation tests

--- ooplab/src/test/java/agh/ics/oop/AnimalMovementIntegrationTest.java
+++ ooplab/src/test/java/agh/ics/oop/AnimalMovementIntegrationTest.java
@@ -11,6 +11,114 @@ import java.util.List;
 class AnimalMovementIntegrationTest {
 
   @Test
+  void shouldChangeAnimalsOrientationToNorth() {
+    //given
+    String[] args = new String[] { "l", "r", "l", "r", "l", "r", "l", "r" };
+    var animalPositions = List.of(new Vector2d(2, 2), new Vector2d(2, 2));
+    var moveDirections = OptionsParser.parse(args);
+
+    //when
+    var simulation = new Simulation(animalPositions, moveDirections);
+    simulation.run();
+
+    //then
+    Assertions.assertEquals(8, moveDirections.size());
+    Assertions.assertEquals(MoveDirection.LEFT, moveDirections.get(0));
+    Assertions.assertEquals(MoveDirection.RIGHT, moveDirections.get(1));
+    Assertions.assertEquals(MoveDirection.LEFT, moveDirections.get(2));
+    Assertions.assertEquals(MoveDirection.RIGHT, moveDirections.get(3));
+    Assertions.assertEquals(MoveDirection.LEFT, moveDirections.get(4));
+    Assertions.assertEquals(MoveDirection.RIGHT, moveDirections.get(5));
+    Assertions.assertEquals(MoveDirection.LEFT, moveDirections.get(6));
+    Assertions.assertEquals(MoveDirection.RIGHT, moveDirections.get(7));
+
+    var firstAnimal = simulation.getAnimals().getFirst();
+    Assertions.assertEquals(MapDirection.NORTH, firstAnimal.getOrientation());
+
+    var secondAnimal = simulation.getAnimals().getLast();
+    Assertions.assertEquals(MapDirection.NORTH, secondAnimal.getOrientation());
+  }
+
+  @Test
+  void shouldChangeAnimalsOrientationToSouth() {
+    //given
+    String[] args = new String[] { "l", "r", "l", "r" };
+    var animalPositions = List.of(new Vector2d(2, 2), new Vector2d(2, 2));
+    var moveDirections = OptionsParser.parse(args);
+
+    //when
+    var simulation = new Simulation(animalPositions, moveDirections);
+    simulation.run();
+
+    //then
+    Assertions.assertEquals(4, moveDirections.size());
+    Assertions.assertEquals(MoveDirection.LEFT, moveDirections.get(0));
+    Assertions.assertEquals(MoveDirection.RIGHT, moveDirections.get(1));
+    Assertions.assertEquals(MoveDirection.LEFT, moveDirections.get(2));
+    Assertions.assertEquals(MoveDirection.RIGHT, moveDirections.get(3));
+
+    var firstAnimal = simulation.getAnimals().getFirst();
+    Assertions.assertEquals(MapDirection.SOUTH, firstAnimal.getOrientation());
+
+    var secondAnimal = simulation.getAnimals().getLast();
+    Assertions.assertEquals(MapDirection.SOUTH, secondAnimal.getOrientation());
+  }
+
+  @Test
+  void shouldChangeAnimalsOrientationToWest() {
+    //given
+    String[] args = new String[] { "l", "r", "f", "r", "f", "r" };
+    var animalPositions = List.of(new Vector2d(2, 2), new Vector2d(2, 2));
+    var moveDirections = OptionsParser.parse(args);
+
+    //when
+    var simulation = new Simulation(animalPositions, moveDirections);
+    simulation.run();
+
+    //then
+    Assertions.assertEquals(6, moveDirections.size());
+    Assertions.assertEquals(MoveDirection.LEFT, moveDirections.get(0));
+    Assertions.assertEquals(MoveDirection.RIGHT, moveDirections.get(1));
+    Assertions.assertEquals(MoveDirection.FORWARD, moveDirections.get(2));
+    Assertions.assertEquals(MoveDirection.RIGHT, moveDirections.get(3));
+    Assertions.assertEquals(MoveDirection.FORWARD, moveDirections.get(4));
+    Assertions.assertEquals(MoveDirection.RIGHT, moveDirections.get(5));
+
+    var firstAnimal = simulation.getAnimals().getFirst();
+    Assertions.assertEquals(MapDirection.WEST, firstAnimal.getOrientation());
+
+    var secondAnimal = simulation.getAnimals().getLast();
+    Assertions.assertEquals(MapDirection.WEST, secondAnimal.getOrientation());
+  }
+
+  @Test
+  void shouldChangeAnimalsOrientationToEast() {
+    //given
+    String[] args = new String[] { "r", "l", "f", "l", "f", "l" };
+    var animalPositions = List.of(new Vector2d(2, 2), new Vector2d(2, 2));
+    var moveDirections = OptionsParser.parse(args);
+
+    //when
+    var simulation = new Simulation(animalPositions, moveDirections);
+    simulation.run();
+
+    //then
+    Assertions.assertEquals(6, moveDirections.size());
+    Assertions.assertEquals(MoveDirection.RIGHT, moveDirections.get(0));
+    Assertions.assertEquals(MoveDirection.LEFT, moveDirections.get(1));
+    Assertions.assertEquals(MoveDirection.FORWARD, moveDirections.get(2));
+    Assertions.assertEquals(MoveDirection.LEFT, moveDirections.get(3));
+    Assertions.assertEquals(MoveDirection.FORWARD, moveDirections.get(4));
+    Assertions.assertEquals(MoveDirection.LEFT, moveDirections.get(5));
+
+    var firstAnimal = simulation.getAnimals().getFirst();
+    Assertions.assertEquals(MapDirection.EAST, firstAnimal.getOrientation());
+
+    var secondAnimal = simulation.getAnimals().getLast();
+    Assertions.assertEquals(MapDirection.EAST, secondAnimal.getOrientation());
+  }
+
+  @Test
   void shouldNotChangeXWhenTryingMoveOutsideMap() {
     //given
     String[] args = new String[] { "l", "r", "f", "f", "f", "f" };
