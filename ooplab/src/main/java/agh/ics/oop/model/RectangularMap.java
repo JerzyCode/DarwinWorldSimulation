63c56dd43a56c54e796d801a6f7fa1af77f8bde1
Jerzy Boksa <jerzyxboksa@gmail.com>
2024-11-25 22:08:43 +0100

    refactor rectangular corners

--- ooplab/src/main/java/agh/ics/oop/model/RectangularMap.java
+++ ooplab/src/main/java/agh/ics/oop/model/RectangularMap.java
@@ -2,27 +2,25 @@ package agh.ics.oop.model;
 
 public class RectangularMap extends AbstractWorldMap {
 
-  private final Vector2d leftBotCorner;
-  private final Vector2d rightTopCorner;
+  private final Boundary boundary;
 
   public RectangularMap(int width, int height) {
     super();
-    rightTopCorner = new Vector2d(width - 1, height - 1);
-    leftBotCorner = new Vector2d(0, 0);
+    boundary = new Boundary(new Vector2d(0, 0), new Vector2d(width - 1, height - 1));
   }
 
   //for testing purposes
   Vector2d getRightTopCorner() {
-    return rightTopCorner;
+    return boundary.rightTopCorner();
   }
 
   @Override
   public boolean canMoveTo(Vector2d position) {
-    return super.canMoveTo(position) && position.follows(leftBotCorner) && position.precedes(rightTopCorner);
+    return super.canMoveTo(position) && position.follows(boundary.leftBottomCorner()) && position.precedes(boundary.rightTopCorner());
   }
 
   @Override
   public Boundary getCurrentBounds() {
-    return new Boundary(leftBotCorner, rightTopCorner);
+    return boundary;
   }
 }
