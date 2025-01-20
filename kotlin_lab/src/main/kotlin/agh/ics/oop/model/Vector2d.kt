c289ae89a865b4f5878d764383561e675113dcc7
Jerzy Boksa <jerzyxboksa@gmail.com>
2025-01-20 20:36:03 +0100

    kotlin part1

---
+++ kotlin_lab/src/main/kotlin/agh/ics/oop/model/Vector2d.kt
@@ -0,0 +1,24 @@
+package agh.ics.oop.model
+
+data class Vector2d(val x: Int, val y: Int) : Comparable<Vector2d> {
+    override fun compareTo(other: Vector2d): Int {
+        return when {
+            x >= other.x && y >= other.y -> 1 // follows
+            x <= other.x && y <= other.y -> -1 // precedes
+            else -> 0
+        }
+    }
+
+    operator fun plus(other: Vector2d) = Vector2d(x + other.x, y + other.y)
+
+    operator fun minus(other: Vector2d) = Vector2d(x - other.x, y - other.y)
+
+    fun upperRight(other: Vector2d) = Vector2d(Math.max(x, other.x), Math.max(y, other.y))
+
+    fun lowerLeft(other: Vector2d) = Vector2d(Math.min(x, other.x), Math.min(y, other.y))
+
+    fun opposite() = Vector2d(-x, -y)
+
+    override fun toString() = "($x, $y)"
+
+}
