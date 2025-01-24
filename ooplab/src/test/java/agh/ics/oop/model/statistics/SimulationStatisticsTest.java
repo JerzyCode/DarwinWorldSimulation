d633c02c4218866c19a30181d1c91ec55779dcdb
Mateusz <mateusz.oracz2001@gmail.com>
2025-01-24 11:52:23 +0100

    SimulationStatisticsTest part1

---
+++ ooplab/src/test/java/agh/ics/oop/model/statistics/SimulationStatisticsTest.java
@@ -0,0 +1,82 @@
+package agh.ics.oop.model.statistics;
+
+import agh.ics.oop.Simulation;
+import agh.ics.oop.SimulationContext;
+import agh.ics.oop.TestConfigurationHelper;
+import agh.ics.oop.model.configuration.SimulationConfiguration;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SimulationStatisticsTest {
+
+    SimulationContext simulationWithAnimals;
+    SimulationContext simulationWithoutAnimals;
+    SimulationContext simulationWithoutPlantsAndFires;
+
+    SimulationStatistics simulationWithAnimalsStatistics;
+    SimulationStatistics simulationWithoutAnimalsStatistics;
+    SimulationStatistics simulationWithoutPlantsAndFiresStatistics;
+
+    @BeforeEach
+    void setUp() {
+        var configuration = TestConfigurationHelper.createFireConfiguration();
+        var configuration2 = TestConfigurationHelper.createEarthConfigurationNoAnimals();
+        var configuration3 = TestConfigurationHelper.createFireConfigurationNoPlantsNoFire();
+
+        this.simulationWithAnimals = new SimulationContext(configuration);
+        this.simulationWithoutAnimals = new SimulationContext(configuration2);
+        this.simulationWithoutPlantsAndFires = new SimulationContext(configuration3);
+
+        this.simulationWithAnimalsStatistics = new SimulationStatistics(simulationWithAnimals);
+        this.simulationWithoutAnimalsStatistics = new SimulationStatistics(simulationWithoutAnimals);
+        this.simulationWithoutPlantsAndFiresStatistics = new SimulationStatistics(simulationWithoutPlantsAndFires);
+    }
+
+
+    @Test
+    void getAnimalInitCount() {
+        // given & when
+        var noAnimalsCount = simulationWithoutAnimalsStatistics.getAnimalCount();
+        var defaultAnimalsCount = simulationWithAnimalsStatistics.getAnimalCount();
+        var animalsOnMapWithoutPlantsCount = simulationWithoutPlantsAndFiresStatistics.getAnimalCount();
+
+        // then
+        assertEquals(0, noAnimalsCount);
+        assertEquals(25, defaultAnimalsCount);
+        assertEquals(10, animalsOnMapWithoutPlantsCount);
+    }
+
+    @Test
+    void getAnimalCountShouldReturnProperCountOfAnimalsEveryday() {
+        for(int i=0; i < 16; i++){
+            assertEquals(10 , simulationWithoutPlantsAndFiresStatistics.getAnimalCount());
+            simulationWithoutPlantsAndFires.handleDayEnds();
+        }
+        assertEquals(0 , simulationWithoutPlantsAndFiresStatistics.getAnimalCount());
+    }
+
+    @Test
+    void getAverageAnimalEnergy() {
+        for(int i=0; i < 15; i++){
+            assertTrue(simulationWithoutPlantsAndFiresStatistics.getAverageAnimalEnergy().isPresent());
+            assertEquals(15 - i, simulationWithoutPlantsAndFiresStatistics.getAverageAnimalEnergy().getAsDouble(), 0.01);
+            simulationWithoutPlantsAndFires.handleDayEnds();
+        }
+
+        assertTrue(simulationWithoutAnimalsStatistics.getAverageAnimalEnergy().isEmpty());
+    }
+
+    @Test
+    void getAverageDeadAnimalTimeLife() {
+    }
+
+    @Test
+    void getAverageAnimalCountOfChildren() {
+    }
+
+    @Test
+    void getMostPopularGenotype() {
+    }
+}
