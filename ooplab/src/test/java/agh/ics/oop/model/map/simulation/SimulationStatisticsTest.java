68c7b00eebb605aa7fdb40b864416bb863dec92f
Mateusz <mateusz.oracz2001@gmail.com>
2025-01-24 10:07:53 +0100

    test init

---
+++ ooplab/src/test/java/agh/ics/oop/model/map/simulation/SimulationStatisticsTest.java
@@ -0,0 +1,34 @@
+package agh.ics.oop.model.map.simulation;
+
+import agh.ics.oop.model.configuration.Configuration;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SimulationStatisticsTest {
+
+//    @BeforeEach
+//    void setUp() {}
+//    }
+
+    @Test
+    void getAnimalCount() {
+    }
+
+    @Test
+    void getAverageAnimalEnergy() {
+    }
+
+    @Test
+    void getAverageDeadAnimalTimeLife() {
+    }
+
+    @Test
+    void getAverageAnimalCountOfChildren() {
+    }
+
+    @Test
+    void getMostPopularGenotype() {
+    }
+}
