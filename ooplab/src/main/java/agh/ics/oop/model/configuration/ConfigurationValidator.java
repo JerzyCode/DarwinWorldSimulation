2fbe3103d51088656333f2713cb30ca4a65ee639
Jerzy Boksa <jerzyxboksa@gmail.com>
2025-01-24 09:59:37 +0100

    change max size of map

--- ooplab/src/main/java/agh/ics/oop/model/configuration/ConfigurationValidator.java
+++ ooplab/src/main/java/agh/ics/oop/model/configuration/ConfigurationValidator.java
@@ -3,8 +3,8 @@ package agh.ics.oop.model.configuration;
 import agh.ics.oop.model.exceptions.WrongConfigurationParameterException;
 
 public class ConfigurationValidator {
-    static final int MAX_HEIGHT = 200;
-    static final int MAX_WIDTH = 400;
+    static final int MAX_HEIGHT = 100;
+    static final int MAX_WIDTH = 200;
     static final int MIN_HEIGHT = 5;
     static final int MIN_WIDTH = 5;
     static final int MAX_PLANT_GROWTH = 500;
