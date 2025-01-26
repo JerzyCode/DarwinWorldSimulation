3366b61b9de9ab97d9f12f1cf647fac57f4d0798
Jerzy Boksa <jerzyxboksa@gmail.com>
2025-01-26 01:17:38 +0100

    fix comparator

--- ooplab/src/main/java/agh/ics/oop/model/util/AnimalComparator.java
+++ ooplab/src/main/java/agh/ics/oop/model/util/AnimalComparator.java
@@ -3,10 +3,8 @@ package agh.ics.oop.model.util;
 import agh.ics.oop.model.elements.Animal;
 
 import java.util.Comparator;
-import java.util.Random;
 
 public class AnimalComparator implements Comparator<Animal> {
-    private final Random random = new Random();
 
     @Override
     public int compare(Animal a1, Animal a2) {
@@ -20,11 +18,6 @@ public class AnimalComparator implements Comparator<Animal> {
             return -ageComparison;
         }
 
-        int childrenComparison = Integer.compare(a1.getChildren().size(), a2.getChildren().size());
-        if (childrenComparison != 0) {
-            return childrenComparison;
-        }
-
-        return random.nextBoolean() ? 1 : -1;
+        return Integer.compare(a1.getChildren().size(), a2.getChildren().size());
     }
 }
